b5a71b5d4b737c76aec69e51206520e74f559778
Vinayak Y B <vinayak.bhustali@gmail.com>
2021-09-11 16:44:24 +0530

    Initial Commit
    The application to check birthday as lucky number is built and deployed

---
+++ script.js
@@ -0,0 +1,34 @@
+const dateOfBirth = document.getElementById("date-of-birth");
+const luckyNumber = document.getElementById("lucky-number");
+const checkNumberButton = document.getElementById("check-number");
+const outputBox = document.getElementById("output-box");
+
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(sum&&dob){
+    compareValues(sum, luckyNumber.value);
+    }else{
+        outputBox.innerText = "Please enter valid input";
+    }
+}
+
+function compareValues(sum, luckyNumber){
+    if(sum%luckyNumber === 0){
+        outputBox.innerText = "Your Birthday is lucky! 😁";
+    }
+    else{
+        outputBox.innerText = "Your Birthday is not lucky.. 🤔";
+    }
+}
+
+function calculateSum (dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let index=0; index < dob.length; index++){
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky)
